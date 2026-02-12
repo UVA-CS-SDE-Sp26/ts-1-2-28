21696ab87d37aecf98f62e608fcb73cc8d6bd3de
Brian Hardy <brian@yachtx.com>
2026-02-11 21:04:03 -0500

    updated cipher interface

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -2,22 +2,19 @@
  * Decryption Class
  */
 interface Decryption {
-    String decrypt (String data);
-    String decrypt (String data, String key2);
+    String decrypt (String data, String key); //run this with the default key too
 }
 
 public class Cipher implements Decryption {
-    public String decrypt (String data){
-        return "";
+    public String decrypt (String data, String key){
+        return ""; //include key validation here...
     }
-    public String decrypt (String data, String key2){
-        return "";
+    private char decryptCharacter (char x, String key){
+        return 'a';
     }
 
-    private char decryptCharacter (char x){
-        return 'a';
-    }
-    private char decryptCharacter (char x, String key2){
-        return 'a';
+    private boolean verifyKey (String key2) {
+
+        return false;
     }
 }

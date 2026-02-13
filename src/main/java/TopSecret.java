4d7939800145b59560693a70626ceec5987da61d
aditib08 <aditiburra@gmail.com>
2026-02-13 15:21:59 -0500

    Fixed argument handling in TopSecret

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -1,38 +1,53 @@
 import java.io.File;
 
-/**
- * Commmand Line Utility
- */
 public class TopSecret {
     public static void main(String[] args) {
+
         if (args.length == 0) { // no arguments = list the available files
-
             ProgramControl.runNoArguments(); // list files to display
-
             return;
-        } else if (args.length == 1) { // 1 argument = show specified file using the default decipher key
+        }
+        else if (args.length == 1) { // 1 argument = show specified file using the default decipher key
             String fileNumber = args[0];
 
             if (!fileNumber.matches("\\d{2}")) {
-                System.out.println("error: file number must be two digits (ex. 01, 10");
+                System.out.println("error: file number must be two digits (ex. 01, 10)");
                 return;
             }
 
-            ProgramControl.runOneArgument(fileNumber); // display file with deciphered text (using default key)
-
+            ProgramControl.runOneArgument(fileNumber); // display file with default key
             return;
-        } else if (args.length == 2) {
+        }
+        else if (args.length == 2) { // 2 arguments = show file with custom key
+            String fileNumber = args[0];
+            String decipherKey = args[1];
 
-            boolean validNumber = args[0].matches("\\d{2}");
-            boolean validFile = args[1].endsWith(".txt");
+            boolean validNumber = fileNumber.matches("\\d{2}");
+            boolean validFile = decipherKey.endsWith(".txt");
 
             if (!validNumber && !validFile) {
                 System.out.println("error: file number must be two digits (ex. 01, 10 AND decipher key must be a string that ends in .txt (ex. key.txt)");
-            } else if (!validNumber) {
+            }
+            else if (!validNumber) {
                 System.out.println("error: file number must be two digits");
-            } else if (!validFile) {
+            }
+            else if (!validFile) {
                 System.out.println("error: decipher key must be a string that ends in .txt (ex. key.txt)");
             }
+            else {
+                ProgramControl.runTwoArguments(fileNumber, new File(decipherKey));
+            }
+
+            return;
+        }
+        else { // more than 2 arguments
+            System.out.println(
+                    "error: too many arguments. options are:\n" +
+                            "java TopSecret\n" +
+                            "java TopSecret [number]\n" +
+                            "java TopSecret [number] [decipherkey.txt]"
+            );
+            return;
         }
     }
 }
